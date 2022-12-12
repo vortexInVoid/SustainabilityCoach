b65b3f4a426a3d1ae35bad42f7f43d1ace403163
Batuhan Balaban <114015378+BatuhanEEE@users.noreply.github.com>
2022-12-12 20:46:14 +0300

    Add files via upload
    closer to final

---
+++ Final/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+//import javafx.scene.layout.BorderPane;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+      
+        coachControl  theControl = new coachControl( );
+        theControl.frameStart( );
+      
+
+      
+      
+    }
+}
